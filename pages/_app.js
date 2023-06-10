1fa0dec0e2d703eb8e7707697e6784db110155dd
Sam7144 <79979572+Sam7144@users.noreply.github.com>
2023-06-10 17:51:12 +0300

    redo redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
 import Layout from "./components/Layout";
-import { Provider } from 'react-redux'
 import { SessionProvider } from "next-auth/react";
-import { store } from "@/store";
 import { StoreProvider } from "@/store2/Store";
 export default function App({ Component, pageProps }) {
   return (
